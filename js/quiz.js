12400fd0815a3ff166ba7744828f354606b1d22f
Troy Rios <troy.rios@gmail.com>
2019-10-11 13:39:05 +0000

    array challenge part 2

--- js/quiz.js
+++ js/quiz.js
@@ -11,22 +11,42 @@ var questions = [
   ['What is 10*10?', 100]
 ];
 
+var correctQuestions = [];
+var wrongQuestions = [];
+
 for(var i = 0; i<questions.length; i+=1)
 { 
   var answer = prompt(questions[i][0]);
   if(parseInt(answer) === questions[i][1])
   {
-     correct +=1;
+    correct +=1;
+    correctQuestions.push(questions[i][0]);
+  }
+  else
+  {
+     wrongQuestions.push(questions[i][0]);                       
   }
     
 }
-if(correct > 1)
-{
-  message = 'You answered ' + correct + ' answers correctly.';
-}
-else
-{
-  message = 'You answered ' + correct + ' answer correctly.';
 
-}
+  message = 'You answered ' + correct + ' answer(s) correctly.<br><br>';
+  if(correctQuestions.length > 0)
+  {
+    message += '<bold>You got these questions correct:</bold><br><ol>'
+    for(var i = 0; i<correctQuestions.length;i+=1)
+    {
+      message += '<li>' + correctQuestions[i] + '</li>'
+    }
+    message += '</ol><br>'
+  }
+  if(wrongQuestions.length >0)
+  {
+    message += '<bold>You got these questions wrong:</bold><br><ol>'
+    for(var i = 0; i<wrongQuestions.length;i+=1)
+    {
+      message += '<li>' + wrongQuestions[i] + '</li>'
+    }
+    message += '</ol>'
+  }
+
 print(message);
