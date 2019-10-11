c1ca047f0157d38e66df2efdb76f5d96e3628c41
Troy Rios <troy.rios@gmail.com>
2019-10-11 13:07:46 +0000

    initial commit

---
+++ js/quiz.js
@@ -0,0 +1,32 @@
+
+function print(message) {
+  document.write(message);
+}
+
+var correct = 0;
+var message = '';
+var questions = [
+  ['What is 5*5?', '25'],
+  ['What is 5*0?', '0' ],
+  ['What is 10*10?', '100']
+];
+
+for(var i = 0; i<questions.length; i+=1)
+{ 
+  var answer = prompt(questions[i][0]);
+  if(answer === questions[i][1])
+  {
+     correct +=1;
+  }
+    
+}
+if(correct > 1)
+{
+  message = 'You answered ' + correct + ' answers correctly.';
+}
+else
+{
+  message = 'You answered ' + correct + ' answer correctly.';
+
+}
+print(message);
